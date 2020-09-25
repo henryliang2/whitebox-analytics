ef6835d566be7ba938fd4e519624431d3ab0c16d
zomgitshenry <henryliang@alumni.ubc.ca>
2020-09-25 00:11:59 -0700

    fixed useless import errors

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -8,17 +8,17 @@ const PortfolioItem = (props) => {
       <div className='portfolio-item'>
         
         { !props.isEven && 
-          <div className='portfolio-item__image'><img src={ props.portfolioImgUrl} /></div>
+          <div className='portfolio-item__image'><img src={ props.portfolioImgUrl} alt={ props.company }/></div>
         }
 
         <div className='portfolio-item__text-container'>
-          <div className='portfolio-item__brand'><img src={ props.brandImgUrl }/></div>
+          <div className='portfolio-item__brand'><img src={ props.brandImgUrl } alt={`${props.company} logo`}/></div>
           <div className='portfolio-item__header'>{ props.company }</div>
           <div className='portfolio-item__text'> { props.text }</div>
         </div>
 
         { props.isEven && 
-          <div className='portfolio-item__image'><img src={ props.portfolioImgUrl} /></div>
+          <div className='portfolio-item__image'><img src={ props.portfolioImgUrl} alt={ props.company }/></div>
         }
 
       </div>
--- src/pages/education.js
+++ src/pages/education.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
 import Navigation from '../components/Navigation';
-import LandingCard from '../components/LandingCard';
 import SEO from "../components/seo";
 import '../styles/app.css';
 
