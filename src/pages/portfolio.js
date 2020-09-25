70e3a855d509cedac65b70a47c1a184fa29028b1
zomgitshenry <henryliang@alumni.ubc.ca>
2020-09-24 19:48:31 -0700

    modified image directories

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -11,19 +11,19 @@ const PortfolioPage = () => {
       'company': 'Lululemon Athletica',
       'text': 'We were consulted by Lululemon Athletica to help improve their technical clothing line, applying machine learning techniques to model the anatomy and biomechanics of the human body.',
       'brandImgUrl': 'lululemon.png',
-      'portfolioImgUrl': 'lululemon-portfolio.jpg'
+      'portfolioImgUrl': './../lululemon-portfolio.jpg'
     },
     {
       'company': 'Translink',
       'text': 'We worked with Translink, the municipal public transport authority for the Vancouver area, to help discover pain points and sources of congestion in Vancouver',
       'brandImgUrl': 'translink.png',
-      'portfolioImgUrl': 'translink-portfolio.jpg'
+      'portfolioImgUrl': './../translink-portfolio.jpg'
     },
     {
       'company': 'Save-On-Foods',
       'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
       'brandImgUrl': 'save-on-foods.svg',
-      'portfolioImgUrl': 'save-on-foods-portfolio.jpg'
+      'portfolioImgUrl': './../save-on-foods-portfolio.jpg'
     },
 
   ]
