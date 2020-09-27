74a5ead8fdee1aa6d4d1e975645f48fcdb04699c
zomgitshenry <henryliang@alumni.ubc.ca>
2020-09-27 13:50:55 -0700

    changed insightcard styling and added contents page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,24 +2,41 @@ import React from "react";
 import Navigation from '../components/Navigation';
 import SEO from "../components/seo";
 import '../styles/app.css';
+import '../styles/contact.css';
+import LogoSquare from './../../public/logo-square.png';
 
 const ContactPage = () => {
 
   return (
-  <div className='container'>
+  <div className='layout__container'>
 
-  <SEO title="Contact" />
-  <Navigation />
+    <SEO title="Contact" />
+    <Navigation />
 
-  <div className='contact__form'>
+      <div className='contact'>
+        <div className='contact__brand-container'>
+          <div className='contact__brand'>
+            <img src={ LogoSquare } alt='logo' />
+          </div>
+        </div>
+        <div className='contact__form'>
 
-    <h2>Contact</h2>
-    <form>
-    
-    </form>
+          <div className='layout__header'>Get in touch with us</div>
+
+          <label>Organization</label>
+          <input type='text'></input>
+
+          <label>Email</label>
+          <input type='text'></input>
+
+          <label>Message</label>
+          <textarea></textarea>
+        </div>
+      </div>
+  
   </div>
 
-  </div>
+  
 
   );
   
