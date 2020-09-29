a404bd0390d2a7d13413ba87d89abe12aac7e3cd
zomgitshenry <henryliang@alumni.ubc.ca>
2020-09-28 22:37:14 -0700

    corrected Footer positioning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,28 +8,24 @@ const Footer = () => {
 
   return (
       <div className='footer'>
-      
-        <div className='layout__container'>
         
-          <div className='footer__inner'>
+        <div className='footer__inner'>
 
-            <img src='./logo-dark.png' alt='Footer Logo'/>
+          <img src='./logo-dark.png' alt='Footer Logo'/>
 
-            <div className='footer__address'>
-              2300 - 550 Burrard St <br/>
-              Vancouver, BC &nbsp;&nbsp;&nbsp; V6C 2B5 <br/><br/>
-              example@gmail.com
-            </div>
+          <div className='footer__address'>
+            2300 - 550 Burrard St <br/>
+            Vancouver, BC &nbsp;&nbsp;&nbsp; V6C 2B5 <br/><br/>
+            example@gmail.com
+          </div>
 
 
-            <div className='footer__links'>
-              <Link to='/portfolio'>Portfolio</Link>
-              <Link to='/insights'>Insights</Link>
-              <Link to='/about'>About</Link>
-              <Link to='/education'>Education</Link>
-              <Link to="/contact">Contact</Link>
-            </div>
-
+          <div className='footer__links'>
+            <Link to='/portfolio'>Portfolio</Link>
+            <Link to='/insights'>Insights</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/education'>Education</Link>
+            <Link to="/contact">Contact</Link>
           </div>
 
         </div>
