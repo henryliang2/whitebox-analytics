b01b2c48923193b2e92a2206a667a52852f22870
zomgitshenry <henryliang@alumni.ubc.ca>
2020-09-28 21:28:43 -0700

    initialized footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import '../styles/app.css'
 import '../styles/Footer.css'
 
@@ -10,8 +11,26 @@ const Footer = () => {
       
         <div className='layout__container'>
         
-        { // contents
-        }
+          <div className='footer__inner'>
+
+            <img src='./logo-dark.png' alt='Footer Logo'/>
+
+            <div className='footer__address'>
+              123 Bautista Road, <br/>
+              Vancouver, BC &nbsp;&nbsp;&nbsp; V1V 1V1 <br/><br/>
+              example@gmail.com
+            </div>
+
+
+            <div className='footer__links'>
+              <Link to='/portfolio'>Portfolio</Link>
+              <Link to='/insights'>Insights</Link>
+              <Link to='/about'>About</Link>
+              <Link to='/education'>Education</Link>
+              <Link to="/contact">Contact</Link>
+            </div>
+
+          </div>
 
         </div>
       
